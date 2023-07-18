f8d644a7df1b3cb1f1522e82555f6f2c306cb250
Zhichao Jin <jinzcdev@icloud.com>
2023-07-18 15:54:19 +0800

    refactor: improve the ui of interfaceInfo

--- src/pages/InterfaceList/index.tsx
+++ src/pages/InterfaceList/index.tsx
@@ -1,67 +1,183 @@
-import {PageContainer} from '@ant-design/pro-components';
-import React, {useEffect, useState} from 'react';
-import {List, message} from "antd";
-import {Link} from "umi";
-import {listInterfaceInfoByPageUsingGET} from "@/services/oneapi/interfaceInfoController";
+import {ActionType, PageContainer, ProColumns, ProTable} from '@ant-design/pro-components';
+import {Button, Layout, message} from 'antd';
+import Search from 'antd/es/input/Search';
+import {Content, Header} from 'antd/es/layout/layout';
+import React, {useEffect, useRef, useState} from 'react';
+import {history} from 'umi';
+import {listInterfaceInfoByPageUsingGET, publishApiUsingPOST} from "@/services/oneapi/interfaceInfoController";
+
+const headerStyle: React.CSSProperties = {
+  textAlign: 'center',
+  height: '64px',
+  paddingInline: '30%',
+  lineHeight: '64px',
+  color: '#fff',
+  background: '#fcfcfc',
+};
+
+const contentStyle: React.CSSProperties = {
+  minHeight: 120,
+  lineHeight: '120px',
+};
 
 
 const Index: React.FC = () => {
   const [loading, setLoading] = useState(false);
   const [list, setList] = useState<API.InterfaceInfo[]>([]);
   const [total, setTotal] = useState<number>(0);
+  const ref = useRef<ActionType>();
 
-  const loadData = async (current = 1, pageSize = 10) => {
+  const loadData = async (searchText = '', current = 1, pageSize = 10) => {
     setLoading(true);
     try {
-      const res = await listInterfaceInfoByPageUsingGET({
-        current, pageSize
+      const params = {
+        current,
+        pageSize
+      };
+      if (searchText) {
+        Object.assign(params, {
+          name: searchText,
+          description: searchText,
+        });
+      }
+      await listInterfaceInfoByPageUsingGET(params).then((res) => {
+        setList(res?.data?.records ?? []);
+        setTotal(res?.data?.total ?? 0);
       });
-      setList(res?.data?.records ?? []);
-      setTotal(res?.data?.total ?? 0)
     } catch (error: any) {
       message.error('请求失败，' + error.message);
     }
     setLoading(false);
-  }
+  };
+
+
+  /**
+   * table 展示的列
+   * */
+  const columns: ProColumns<API.InterfaceInfo>[] = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      valueType: 'index',
+      align: 'center',
+    },
+    {
+      title: '接口名称',
+      dataIndex: 'name',
+      valueType: 'text',
+      align: 'center',
+    },
+    {
+      title: '描述',
+      dataIndex: 'description',
+      valueType: 'textarea',
+      align: 'center',
+    },
+    {
+      title: '请求方法',
+      dataIndex: 'method',
+      valueType: 'text',
+      align: 'center',
+    },
+    {
+      title: '状态',
+      dataIndex: 'status',
+      hideInForm: true,
+      valueEnum: {
+        0: {
+          text: '关闭',
+          status: 'Default',
+        },
+        1: {
+          text: '开启',
+          status: 'Processing',
+        },
+      },
+      align: 'center',
+    },
+    {
+      title: '发布时间',
+      dataIndex: 'createTime',
+      valueType: 'dateTime',
+      align: 'center',
+    },
+    {
+      title: '操作',
+      dataIndex: 'option',
+      valueType: 'option',
+      render: (_, record) => {
+        return true ? (
+          <Button
+            type="primary"
+            key="onlineUse"
+            onClick={() => {
+              history.push(`/interface-info/${record.id}`);
+            }}
+          >
+            在线调用
+          </Button>
+        ) : (
+          <Button
+            key="applyInterface"
+            onClick={async () => {
+              const res = await publishApiUsingPOST({
+                id: Number(record.id),
+              });
+              if (res.code === 0) {
+                message.success('申请成功');
+                // 刷新表格
+                await loadData();
+              }
+            }}
+          >
+            开通接口
+          </Button>
+        );
+      },
+    },
+  ];
 
   useEffect(() => {
     loadData();
-  }, [])
+  }, []);
+
+  const onSearch = (value: string) => {
+    loadData(value);
+  };
 
   return (
-    <PageContainer title={"在线接口开放平台"}>
-      <List
-        className="interfaceList"
-        loading={loading}
-        itemLayout="horizontal"
-        dataSource={list}
-        renderItem={
-          (item) => {
-            const apiLink = `/interface_info/${item.id}`;
-            return <List.Item
-              actions={[<Link key={item.id} to={apiLink}>查看</Link>]}
-            >
-              <List.Item.Meta
-                title={<Link to={apiLink}>{item.name}</Link>}
-                description={item.description}
-              />
-            </List.Item>
-          }
-        }
-        pagination={
-          {
-            // eslint-disable-next-line @typescript-eslint/no-shadow
-            showTotal(total: number) {
-              return '总数:' + total
-            },
-            pageSize: 10,
-            total,
-            onChange(page, pageSize) {
-              loadData(page, pageSize);
-            }
-          }
-        }
-      />
+    <PageContainer>
+      <Layout>
+        <Header style={headerStyle}>
+          <Search
+            size={'large'}
+            placeholder="请输入接口名称或描述"
+            onSearch={onSearch}
+            enterButton
+          />
+        </Header>
+        <Content style={contentStyle}>
+          <ProTable<API.InterfaceInfo>
+            rowKey="id"
+            toolBarRender={false}
+            columns={columns}
+            dataSource={list}
+            loading={loading}
+            actionRef={ref}
+            pagination={{
+              showTotal: (total) => {
+                return '总数：' + total;
+              },
+              total,
+              pageSize: 10,
+              onChange: (page, pageSize) => {
+                loadData('', page, pageSize);
+              },
+            }}
+            search={false}
+          />
+        </Content>
+      </Layout>
     </PageContainer>
   );
 };
