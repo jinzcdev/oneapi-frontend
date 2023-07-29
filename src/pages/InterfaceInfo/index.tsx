9edb5c47e2226c5abcec1c65b4aadd3355a006e5
Zhichao Jin <jinzcdev@icloud.com>
2023-07-29 17:39:36 +0800

    feat: add the API call online

--- src/pages/InterfaceInfo/index.tsx
+++ src/pages/InterfaceInfo/index.tsx
@@ -1,157 +1,204 @@
+import {useParams} from '@@/exports';
 import {PageContainer} from '@ant-design/pro-components';
+import {Badge, Button, Card, Descriptions, Divider, Form, Input, message, Table} from 'antd';
+import type {ColumnsType} from 'antd/es/table';
+import moment from 'moment';
 import React, {useEffect, useState} from 'react';
-import {useParams} from "@@/exports";
-import {getInterfaceInfoByIdUsingGET, invokeInterfaceInfoUsingPOST} from "@/services/oneapi/interfaceInfoController";
-import {Badge, Button, Card, Descriptions, Form, Image, Input, message, Space} from "antd";
-import moment from "moment";
-import {defaultStyles, JsonView} from "react-json-view-lite";
+import {getInterfaceInfoVOByIdUsingGET, invokeInterfaceInfoUsingPOST} from "@/services/oneapi/interfaceInfoController";
 
 
-const InterfaceInfo: React.FC = () => {
-  const [loading, setLoading] = useState(false);
-  const [interfaceInfo, setInterfaceInfo] = useState<API.InterfaceInfo | undefined>(undefined);
+// use the component in your app!
 
-  const [apiResult, setApiResult] = useState<string>("{}");
-  const params = useParams<{ id: string }>();
+const requestColumns: ColumnsType<API.RequestParamsRemarkVO> = [
+  {
+    title: '名称',
+    dataIndex: 'name',
+    width: '100px',
+  },
+  {
+    title: '必填',
+    key: 'isRequired',
+    dataIndex: 'isRequired',
+    width: '100px',
+  },
+  {
+    title: '类型',
+    dataIndex: 'type',
+    width: '100px',
+  },
+  {
+    title: '说明',
+    dataIndex: 'remark',
+  },
+];
+const responseColumns: ColumnsType<API.RequestParamsRemarkVO> = [
+  {
+    title: '名称',
+    dataIndex: 'name',
+    width: '100px',
+  },
+  {
+    title: '类型',
+    dataIndex: 'type',
+    width: '100px',
+  },
+  {
+    title: '说明',
+    dataIndex: 'remark',
+  },
+];
+const Index: React.FC = () => {
+  const [invokeLoading, setInvokeLoading] = useState(false);
+  const [data, setData] = useState<API.InterfaceInfoVO>();
+  const params = useParams();
+  const [invokeRes, setInvokeRes] = useState<any>();
 
-  const handleInvoke = async (value: API.InterfaceInfoInvokeRequest) => {
-    const hide = message.loading('调用中...');
+  const loadData = async () => {
     try {
-      const res = await invokeInterfaceInfoUsingPOST(value);
-      hide();
-      if (res && res.code === 0) {
-        message.success('调用成功');
-        setApiResult(res.data ?? "");
-        return true;
-      }
-      message.error('调用失败：' + res.message);
-      return false;
-    } catch (error) {
-      hide();
-      message.error('调用失败：' + error);
-      return false;
-    }
-  };
+      const interfaceInfoRes = await getInterfaceInfoVOByIdUsingGET({
+        id: Number(params.id),
+      });
 
-  const loadData = async (): Promise<API.InterfaceInfo | undefined> => {
-    setLoading(true);
-    const res = await getInterfaceInfoByIdUsingGET({id: Number(params.id)});
-    try {
-      if (res.code === 0 && res.data) {
-        setLoading(false);
-        return res.data;
+      const interfaceInfoData = interfaceInfoRes.data;
+
+      if (interfaceInfoData) {
+        setData(interfaceInfoData);
       }
-    } catch (e) {
-      message.error('请求失败！');
+    } catch (error: any) {
+      message.error('请求失败，' + error.message);
     }
-    setLoading(false);
-    return undefined;
   };
 
   useEffect(() => {
-    loadData().then(data => {
-      setInterfaceInfo(data);
-    });
-  }, [])
+    loadData();
+  }, []);
+
+  const onFinish = async (values: any) => {
+    if (!params.id) {
+      message.error('接口不存在');
+      return;
+    }
+    setInvokeLoading(true);
+    try {
+      const res = await invokeInterfaceInfoUsingPOST({
+        id: params.id,
+        ...values,
+      });
+      console.log('调用接口请求数据：', res);
+      if (res.data) {
+        res.data = res.data.replace(/\\/g, '');
+        // const jsonData = JSON.stringify(res.data as unknown as string);
+        setInvokeRes(res.data);
+        message.success('接口请求成功');
+      } else {
+        setInvokeRes(JSON.stringify(res));
+        message.error(res.message);
+      }
+    } catch (error: any) {
+      message.error('接口请求失败');
+    }
+    setInvokeLoading(false);
+  };
 
   return (
-    <PageContainer title={"接口详情"} loading={loading}>
-      <Space direction="vertical" size={20}>
-        <Card title={interfaceInfo?.name}>
-          {
-            interfaceInfo ?
-              <Descriptions bordered>
-                <Descriptions.Item label="请求地址">{interfaceInfo.url}</Descriptions.Item>
-                <Descriptions.Item label="描述" span={2}>{interfaceInfo.description}</Descriptions.Item>
-                <Descriptions.Item label="请求方法">{interfaceInfo.method}</Descriptions.Item>
-                <Descriptions.Item
-                  label="创建时间">{moment(interfaceInfo.createTime).format('YYYY-MM-DD HH:mm:ss')}</Descriptions.Item>
-                <Descriptions.Item label="更新时间" span={2}>
-                  {moment(interfaceInfo.updateTime).format('YYYY-MM-DD HH:mm:ss')}
-                </Descriptions.Item>
-                <Descriptions.Item label="接口状态" span={3}>
-                  <Badge status={interfaceInfo.status === 1 ? "success" : "error"}
-                         text={interfaceInfo.status === 1 ? "正常" : "下线"}/>
-                </Descriptions.Item>
-                <Descriptions.Item label="请求头">{interfaceInfo.requestHeader}</Descriptions.Item>
-                <Descriptions.Item label="响应头">{interfaceInfo.responseHeader}</Descriptions.Item>
-                <Descriptions.Item label="价格">免费</Descriptions.Item>
-                <Descriptions.Item label="请求参数">
-                  {
-                    interfaceInfo.method === "GET" &&
-                    ((interfaceInfo.requestParams?.trim() ?? "") === "" ? "无" : (interfaceInfo.url ?? "" + interfaceInfo.requestParams))
-                  }
-                  {
-                    interfaceInfo.method === "POST"
-                    && interfaceInfo.requestParams ?
-                      <JsonView
-                        data={JSON.parse(interfaceInfo.requestParams)}
-                        shouldInitiallyExpand={() => true}
-                        style={defaultStyles}/>
-                      : ""
-                  }
-                </Descriptions.Item>
-              </Descriptions> : ""
-          }
-        </Card>
-
-        <Card title={'在线测试'}>
-          <Form
-            name="invoke"
-            layout={"vertical"}
-            onFinish={async (values) => {
-              if (!interfaceInfo) {
-                message.error("接口信息不存在");
-                return;
+    <PageContainer>
+      <Card>
+        {data ? (
+          <Descriptions title={data.name} column={4} layout={'vertical'}>
+            <Descriptions.Item label="描述">{data.description}</Descriptions.Item>
+            <Descriptions.Item label="接口状态">
+              {data.status ? (
+                <Badge status="success" text={'开启'}/>
+              ) : (
+                <Badge status="default" text={'关闭'}/>
+              )}
+            </Descriptions.Item>
+            <Descriptions.Item label="主机名">{data.host}</Descriptions.Item>
+            <Descriptions.Item label="请求地址">{data.url}</Descriptions.Item>
+            <Descriptions.Item label="请求方法">{data.method}</Descriptions.Item>
+            <Descriptions.Item label="请求参数示例" span={4}>
+              {data.requestParams ?? "无需附带参数"}
+            </Descriptions.Item>
+            <Descriptions.Item label="请求参数说明" span={4}>
+              {
+                data.requestParamsRemark && data.requestParamsRemark.length > 0 ?
+                  <Table
+                    style={{width: '100%'}}
+                    pagination={{
+                      hideOnSinglePage: true,
+                    }}
+                    columns={requestColumns}
+                    dataSource={data.requestParamsRemark}
+                  /> : "无"
               }
-              await handleInvoke({
-                id: interfaceInfo.id,
-                requestParams: values.params,
-              });
-            }}
-            onFinishFailed={() => {
-            }}
-          >
-            {
-              interfaceInfo?.requestParams ? <>
-                  <Form.Item
-                    label="请求参数"
-                    name="params"
-                  >
-                    <Input/>
-                  </Form.Item>
-                </> :
-                <div style={{
-                  marginBottom: 16
-                }}>请求参数：无</div>
+            </Descriptions.Item>
+            <Descriptions.Item label="响应参数说明" span={4}>
+              {
+                data.responseParamsRemark && data.responseParamsRemark.length > 0 ?
+                  <Table
+                    style={{width: '100%'}}
+                    pagination={{
+                      hideOnSinglePage: true,
+                    }}
+                    columns={responseColumns}
+                    dataSource={data.responseParamsRemark}
+                  /> : "无"
+              }
+            </Descriptions.Item>
+            <Descriptions.Item label="请求头">{data.requestHeader ?? "无"}</Descriptions.Item>
+            <Descriptions.Item label="响应头">{data.responseHeader ?? "无"}</Descriptions.Item>
+            <Descriptions.Item label="创建时间">
+              {moment(data.createTime).format('YYYY-MM-DD HH:mm:ss')}
+            </Descriptions.Item>
+            <Descriptions.Item label="更新时间">
+              {moment(data.updateTime).format('YYYY-MM-DD HH:mm:ss')}
+            </Descriptions.Item>
+          </Descriptions>
+        ) : (
+          <>接口不存在</>
+        )}
+      </Card>
+      {data ? (
+        <>
+          {' '}
+          <Divider/>
+          <Card title={'在线测试'}>
+            <Form name="invoke" layout={'vertical'} onFinish={onFinish}>
+              <Form.Item
+                label={'请求参数'}
+                initialValue={data.requestParams}
+                name={'requestParams'}
+              >
+                {
+                  (!data.requestParams || data.requestParams.trim().length === 0) ?
+                    <Input.TextArea rows={1} disabled/>
+                    :
+                    <Input.TextArea defaultValue={data?.requestParams} autoSize={{
+                      minRows: 1,
+                      maxRows: 8
+                    }}/>
+                }
 
-            }
-            <Button type="primary" htmlType="submit">
-              调用
-            </Button>
+              </Form.Item>
 
-          </Form>
-        </Card>
-
-        <Card title={'返回结果'}>
-          {
-            // 如果返回的结果是图片地址，则使用 Image 组件展现
-            // todo 应该改为使用特定的结果展示类型（如数据库中记录如何展示）来展示，而不是根据结果解析
-            (apiResult.endsWith(".jpg") || apiResult.endsWith(".png")) ?
-              <Image
-                width={200}
-                src={apiResult}
-              /> :
-              apiResult
-            // < JsonView
-            //   data={JSON.parse(apiResult)}
-            //   shouldInitiallyExpand={() => true}
-            //   style={defaultStyles}/>
-          }
-        </Card>
-      </Space>
+              <Form.Item wrapperCol={{span: 16}}>
+                <Button type="primary" htmlType="submit" disabled={invokeLoading}>
+                  调用
+                </Button>
+              </Form.Item>
+            </Form>
+          </Card>
+          <Divider/>
+          <Card title={'返回结果'} loading={invokeLoading}>
+            <Input.TextArea value={invokeRes} autoSize={{
+              minRows: 1,
+              maxRows: 8
+            }}/>
+          </Card>
+        </>
+      ) : null}
     </PageContainer>
   );
 };
 
-export default InterfaceInfo;
+export default Index;
